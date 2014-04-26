f193cd413bc73fc752f23f7d3c9a8e266dc47de7
Bodey Baker <bodey.baker@gmail.com>
2014-04-26 13:14:18 +0900

    settings handles arbitary plans

--- app/scripts/views/settings.js
+++ app/scripts/views/settings.js
@@ -26,44 +26,49 @@ jangleFit.Views = jangleFit.Views || {};
             this.render();
         },
 
-        // TODO: Horrible hack
-        setLevel: function() {
-            var level = jangleFit.user.getProgress('5BX').getRungName();
-            if (level) {
-                var option = this.$el.find('option:contains("'+level+'")');
-                if (option.length !== 1) {
-                    option = option.filter(
-                        function(o, b) {
-                            return b.innerText===level;
-                        }
-                    );
-                }
-                option.attr('selected', true);
-            }
-        },
-
         render: function () {
-            this.$el.html(this.template(jangleFit.user.toJSON()));
-            this.setLevel();
+            var json = jangleFit.user.toJSON();
+            json.plans.forEach(function(plan) {
+                var progress = jangleFit.user.getProgress(plan.id),
+                ladder;
+                if (!progress) {
+                    return;
+                }
+                ladder = progress.getLadder();
+                if (!ladder) {
+                    return;
+                }
+                plan.rungs = ladder.collection.map(
+                    function (item) {
+                        var result = {id: item.id};
+                        if (result.id === progress.getRungName()) {
+                            result.selected = true;
+                        }
+                        return result;
+                    }
+                );
+            });
+            this.$el.html(this.template(json));
             return this;
         },
 
         selectChanged: function(e){
-            var field = $(e.currentTarget);
-            var value = $('option:selected', field).val();
-            var data = {};
-            data[field.attr('id')] = value;
-//TODO: HACK
-            jangleFit.user.getProgress('5BX').setRungName(value);
+            var field = $(e.currentTarget),
+            value = $('option:selected', field).val(),
+            id = field.attr('id'),
+            planId = id.substring('level-'.length),
+            data = {};
+            data[id] = value;
+            jangleFit.user.getProgress(planId).setRungName(value);
             jangleFit.user.save();
-            this.setLevel();
         },
  
         fieldChanged: function(e){
-            var field = $(e.currentTarget);
-            var formGroup = field.parent().parent();
+            var field = $(e.currentTarget),
+            formGroup = field.parent().parent(),
+            data;
             if (field[0].checkValidity()) {
-                var data = {};
+                data = {};
                 data[field.attr('id')] = field.val();
                 jangleFit.user.set(data);
                 formGroup[0].classList.remove('has-error');
